1b8320ae761fde1a6caae54fc0a3ed7bb07cdc25
Epharus Ndumia <epharusndumia@gmail.com>
2024-10-22 07:04:25 +0300

    initial commit

---
+++ starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+//hoisting
+
+//this keyword
+//the this keywrd is not static and depends on how the function is called.
+//the this keyword points at the 'owner' of the function in which the this keyword is used
+//its value is only assigned when the function is actually called
